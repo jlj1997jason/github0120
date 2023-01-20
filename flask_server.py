64a5255d139df245252e5af6be28f259a62d6c36
jlj1997jason <jason1997_06@yahoo.com.tw>
2023-01-21 03:57:19 +0800

    add requirement /n add ssl

--- flask_server.py
+++ flask_server.py
@@ -1,25 +1,46 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
+# enable CORS
+CORS(app)
+
 # Required
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "test"
-# Extra configs, optional:
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/path/to/ca-file"}}  # https://mysqlclient.readthedocs.io/user_guide.html#functions-and-attributes
+# app.config["MYSQL_USER"] = "root"
+# app.config["MYSQL_PASSWORD"] = ""
+# app.config["MYSQL_DB"] = "test"
+# # Extra configs, optional:
+# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+# app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/path/to/ca-file"}}  # https://mysqlclient.readthedocs.io/user_guide.html#functions-and-attributes
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
-@app.route("/")
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute("""SELECT user, host FROM mysql.user""")
-    rv = cur.fetchall()
-    return str(rv)
+# @app.route("/")
+# def users():
+#     cur = mysql.connection.cursor()
+#     cur.execute("""SELECT user, host FROM mysql.user""")
+#     rv = cur.fetchall()
+#     return str(rv)
+
+Article = [
+    {
+        'title':'測試文章1標題',
+        'author':'高能兒',
+        'content':'test content',
+    },{
+        'title':'測試文章2標題',
+        'author':'高能兒2',
+        'content':'test content2',
+    }
+]
+
+@app.route("/",method=['GET'])
+def home():
+    return "Hello World"
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
